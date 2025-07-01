558ca44fa5732c34f815e72c370ac5f035d5ed4f
Pacotacou <p4c0taco@gmail.com>
2025-07-01 18:42:45 -0500

    feat: refactor

--- install.sh
+++ install.sh
@@ -83,61 +83,7 @@ systemctl enable bluetooth
 check_success "Bluetooth service enabled"
 
 systemctl enable cups
-check_success "CUPS service enabled# update
-sudo pacman -Syu
-
-# enable yay
-mkdir ~/Themes
-cd ~/Themes
-sudo pacman -S --needed base-devel git
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-# main settings
-sudo pacman -S system-config-printer sane-airscan gutenprint cups \
-gnome-browser-connector gufw power-profiles-daemon ntfs-3g \
-noto-fonts-cjk adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts
-
-systemctl enable bluetooth
-systemctl enable cups
-systemctl enable ufw
-
-# themes
-cd ~/Themes
-sudo pacman -S --needed gtk-engine-murrine sassc
-
-## colloid theme
-git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
-cd Colloid-gtk-theme
-./install.sh -l --tweaks rimless
-sudo ./install --tweaks rimless
-cd ..
-
-## colloid icons
-git clone https://github.com/vinceliuice/Colloid-icon-theme.git
-cd Colloid-icon-theme
-./install 
-cd ..
-
-## cursor
-git clone https://github.com/vinceliuice/WhiteSur-cursors.git
-cd WhiteSur-cursors
-./install 
-cd ..
-
-## set themes
-gsettings set org.gnome.desktop.interface gtk-theme "Colloid-Dark"
-gsettings set org.gnome.desktop.interface icon-theme "Colloid-Dark"
-gsettings set org.gnome.desktop.interface cursor-theme "WhiteSur-cursors"
-gsettings set org.gnome.shell.extensions.user-theme name "Colloid-Dark"
-
-## flatpak themes
-git clone https://github.com/refi64/stylepak.git
-cd stylepak
-sudo pacman -S --needed ostree appstream-glib
-./stylepak install-system
-./stylepak install-user"
+check_success "CUPS service enabled"
 
 systemctl enable ufw
 check_success "UFW firewall service enabled"
@@ -233,7 +179,7 @@ cd ..
 
 print_status "Applying themes"
 gsettings set org.gnome.desktop.interface gtk-theme "Colloid-Dark"
-gsettings set org.gnome.desktop.interface icon-theme "Colloid"
+gsettings set org.gnome.desktop.interface icon-theme "Colloid-Dark"
 gsettings set org.gnome.desktop.interface cursor-theme "WhiteSur-cursors"
 gsettings set org.gnome.shell.extensions.user-theme name "Colloid-Dark"
 check_success "Themes applied to GNOME"
@@ -265,26 +211,8 @@ check_success "Stylepak user integration"
 cd ..
 
 ########################
-# Additional Software  #
-########################
-
-print_status "Installing additional recommended software"
-
-# Common applications - uncomment what you need
-BROWSERS="firefox"  # Add chromium, vivaldi-stable if needed
-TERMINALS="alacritty kitty"  # Choose your preferred terminal
-FILE_MANAGERS="thunar"  # Or nautilus, nemo, etc.
-EDITORS="code vim"  # Visual Studio Code and vim
-UTILITIES="flameshot timeshift btop"  # Screenshot, backup, system monitor
-
-# Uncomment to install additional software
-# sudo pacman -S --needed --noconfirm $BROWSERS $TERMINALS $FILE_MANAGERS $EDITORS $UTILITIES
-# check_success "Additional software installation"
-
-# You can add more categories or specific software as needed
-# MEDIA="vlc mpv"
-# OFFICE="libreoffice-fresh"
-# GAMING="steam lutris"
+# Completion          #
+#######################
 
 print_status "Setup complete!"
 echo "Please reboot your system to ensure all changes take effect."
